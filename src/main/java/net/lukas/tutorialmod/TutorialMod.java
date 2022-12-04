e6684fd1deb25ea75ebd883324b53b4683c23fb4
LuxxDevs <112978214+LuxxDevs@users.noreply.github.com>
2022-12-04 22:16:13 +0100

    Added Tanzanite Ore and Blocks more are comming soon

--- src/main/java/net/lukas/tutorialmod/TutorialMod.java
+++ src/main/java/net/lukas/tutorialmod/TutorialMod.java
@@ -1,16 +1,19 @@
 package net.lukas.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.lukas.tutorialmod.block.ModBlocks;
+import net.lukas.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//Very impoertad
+//Very Important Comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/lukas/tutorialmod/item/custom/EightBallItem.java
@@ -0,0 +1,4 @@
+package net.lukas.tutorialmod.item.custom;
+
+public class EightBallItem {
+}
